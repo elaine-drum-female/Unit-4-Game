4d6504b55ac91f03c17424bcfe4a6db5fbbd80dc
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2018-12-18 13:45:21 -0600

    added all variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// get variable elements
+
+var winsCounter = 0; // wins
+var lossesCounter = 0; // losses
+var totalScore; // total score
+
+var numList = []; // Array displaying number
+var chosenNum; // Chosen number
+var randomNumber; // random number
+
+varallNumbers = []; // stores all numbers guessed
+varcorrectNum = []; // correct number
+var wrongNum = []; // wrong number
+
+
