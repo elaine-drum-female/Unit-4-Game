732fff053004fcb774ac6d2cdad4ee8b28e4ec86
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2018-12-20 17:52:28 -0600

    updated the script with modifications

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,48 +2,72 @@
 
 var winsCounter = 0; // wins
 var lossesCounter = 0; // losses
-var totalScore = null; // total score
+var totalScore = 0; // total score
 var images;
 
-var numList = []; // Array displaying number
-var chosenNum = null; // Chosen number
+
 var randomNumbers; // random number
 
-varallNumbers = []; // stores all numbers guessed
-varcorrectNum = []; // correct number
-var wrongNum = []; // wrong number
 
-var counter = 0; // counter
-var scoreNumber;
+
+var scoreNumber = 0;
 
 
 // start the Game
 
 $(document).ready(function () {
 
+    $('.images').on('click', function () {
+        var crystalPoints = $(this).data('value');
+        console.log(crystalPoints);
+        scoreNumber += crystalPoints;
+
+        if (scoreNumber === randomResult) {
+            winsCounter++;
+            $('#winnercaption').text("YOU WIN!");
+            $('#wins').text(winsCounter);
+            //(add the winnerscaption and wins text)
+            startGame();
+
+        } else if (scoreNumber > randomResult) {
+            lossesCounter++;
+            $('#losses').text(lossesCounter);
+            //(add lose text)
+            startGame();
+        } else {
+            $('#scoreNum').text(scoreNumber);
+        }
+
+        /*when image is clicked, retrieve the data- value 
+        and then add the value to current score
+        check current score against target score
+        either the score is equal and they win 
+        or the current score is greater than target score and they lose
+        or keep updating the DOM and they keep playing 
+        */
+    });
+
+
+    $('.images').hover(function () {
+
+        $(this).css("cursor", "pointer");
+    });
+
     function startGame() {
+
+        scoreNumber = 0;
+        $('#scoreNum').text(scoreNumber);
+
+        $('#red').attr('data-value', Math.floor(Math.random() * 11) + 1)
+        $('#blue').attr('data-value', Math.floor(Math.random() * 11) + 1)
+        $('#yellow').attr('data-value', Math.floor(Math.random() * 11) + 1)
+        $('#green').attr('data-value', Math.floor(Math.random() * 11) + 1)
 
         randomResult = Math.floor(Math.random() * 60) + 30;
 
         $('#randomNum').html(+randomResult);
 
-
-        for (var i = 0; i < 4; i++) {
-
-            $('.images').hover(function () {
-
-                $(this).css("cursor", "pointer");
-            });
-        }
-
-        $('.images').on('click', function () {
-            randomNumbers = Math.floor(Math.random() * 11) + 1;
-            //console.log(randomNumbers);
-            scoreNumber = $('#scoreNum');
-            $('#scoreNum').html(randomNumbers); 
-
-        });
-
+        randomNumbers = Math.floor(Math.random() * 11) + 1;
 
         //console.log(pickNumber);
     };
